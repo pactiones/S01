880792fbc5133c5a0d72be1f007f296323b43b56
Mateus Pactiones <mateuspactiones@gmail.com>
2026-09-11 11:25:58 -0300

    Exercicio 1 - Relatorio 5

---
+++ Relatorio 5/Exercicio1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int combinar_equipes(int n){
+    if (n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    else if (n > 1){
+        return combinar_equipes(n - 1) + combinar_equipes(n - 2);
+    }
+}
+int main() 
+{
+    int n;
+
+    cout << "Digite o tamanho do chaveamento: " << endl;
+    cin >> n;
+
+    cout << "Total de cenários de confrontos possíveis: " << combinar_equipes(n);
+
+    return 0;
+}
